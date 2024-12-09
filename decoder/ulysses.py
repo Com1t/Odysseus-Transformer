079504830071ff902b6c52ad7badb74ebd146882
Fu-Chiang Chang <fuchiang137@gmail.com>
2024-12-09 08:42:29 +0800

    Rewrite ulysses.py to improve code clarity

--- decoder/ulysses.py
+++ decoder/ulysses.py
@@ -1,115 +1,144 @@
 # copy from https://github.com/jzhang38/EasyContext/blob/main/easy_context/ulysses_attn/prepare_inputs.py
 
-import transformers
-from typing import List, Optional, Tuple, Union
-import warnings
 import torch
 import torch.utils.checkpoint
-
+from typing import List, Optional, Tuple, Union
+import warnings
+import transformers
+from transformers.cache_utils import Cache
+from transformers.models.llama.modeling_llama import repeat_kv, apply_rotary_pos_emb
+from transformers.utils import logging
 try:
     from yunchang.ulysses import UlyssesAttention
+    from yunchang.kernels import FlashAttentionImpl, select_flash_attn_impl
 
-    ulysses_attn = UlyssesAttention()
+    ulysses_attn = UlyssesAttention(attn_type=FlashAttentionImpl.TORCH)
 except:
     print(
         "If you want to use the UlyssesAttention class, please install the yunchang package."
     )
     ulysses_attn = None
 
+logger = logging.get_logger(__name__)
 
-def new_flash_attn_forward(
-    self,
+def ulysses_forward(
     query_states,
     key_states,
     value_states,
-    attention_mask,
-    query_length,
-    dropout=0.0,
+    attn_mask=None,
+    dropout_p=0.0,
+    is_causal=False,
     softmax_scale=None,
     use_sliding_windows=False,
 ):
-    if not self._flash_attn_uses_top_left_mask:
-        causal = self.is_causal
-    else:
-        causal = self.is_causal and query_length != 1
-
     # Contains at least one padding token in the sequence
-    assert attention_mask is None
-    assert causal is True
+    assert attn_mask is None
     assert use_sliding_windows is False
     attn_output = ulysses_attn(
         query_states,
         key_states,
         value_states,
-        dropout,
+        dropout_p,
         softmax_scale,
-        causal=causal,
+        causal=is_causal,
     )
 
     return attn_output
 
-
-def new_decoder_forward(
+def new_attn_forward(
     self,
     hidden_states: torch.Tensor,
     attention_mask: Optional[torch.Tensor] = None,
     position_ids: Optional[torch.LongTensor] = None,
-    past_key_value: Optional[Tuple[torch.Tensor]] = None,
-    output_attentions: Optional[bool] = False,
-    use_cache: Optional[bool] = False,
+    past_key_value: Optional[Cache] = None,
+    output_attentions: bool = False,
+    use_cache: bool = False,
     cache_position: Optional[torch.LongTensor] = None,
+    position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # will become mandatory in v4.46
     **kwargs,
-) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
-    assert isinstance(
-        self.self_attn, transformers.models.llama.modeling_llama.LlamaFlashAttention2
-    ) or isinstance(
-        self.self_attn,
-        transformers.models.mistral.modeling_mistral.MistralFlashAttention2,
-    ), "Please toggle on the Flash Attention 2 implementation when using zigzag ring attention monkey patch."
-
-    if "padding_mask" in kwargs:
-        warnings.warn(
-            "Passing `padding_mask` is deprecated and will be removed in v4.37. Please make sure use `attention_mask` instead.`"
+) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+    if output_attentions:
+        # TODO: Improve this warning with e.g. `model.config.attn_implementation = "manual"` once this is implemented.
+        logger.warning_once(
+            "LlamaModel is using LlamaSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, "
+            'but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
+        )
+        return super().forward(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+            cache_position=cache_position,
+            position_embeddings=position_embeddings,
         )
 
-    residual = hidden_states
+    bsz, q_len, _ = hidden_states.size()
 
-    hidden_states = self.input_layernorm(hidden_states)
+    query_states = self.q_proj(hidden_states)
+    key_states = self.k_proj(hidden_states)
+    value_states = self.v_proj(hidden_states)
 
-    # Self Attention
-    hidden_states, self_attn_weights, present_key_value = self.self_attn(
-        hidden_states=hidden_states,
-        attention_mask=attention_mask,
-        position_ids=position_ids,
-        past_key_value=past_key_value,
-        output_attentions=output_attentions,
-        use_cache=use_cache,
-        cache_position=cache_position,
-        **kwargs,
+    # use -1 to infer num_heads and num_key_value_heads as they may vary if tensor parallel is used
+    query_states = query_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+    key_states = key_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+    value_states = value_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+
+    if position_embeddings is None:
+        logger.warning_once(
+            "The attention layers in this model are transitioning from computing the RoPE embeddings internally "
+            "through `position_ids` (2D tensor with the indexes of the tokens), to using externally computed "
+            "`position_embeddings` (Tuple of tensors, containing cos and sin). In v4.46 `position_ids` will be "
+            "removed and `position_embeddings` will be mandatory."
+        )
+        cos, sin = self.rotary_emb(value_states, position_ids)
+    else:
+        cos, sin = position_embeddings
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+    if past_key_value is not None:
+        # sin and cos are specific to RoPE models; cache_position needed for the static cache
+        cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+        key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+    key_states = repeat_kv(key_states, self.num_key_value_groups)
+    value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+    causal_mask = attention_mask
+    if attention_mask is not None:
+        causal_mask = causal_mask[:, :, :, : key_states.shape[-2]]
+
+    # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+    # Reference: https://github.com/pytorch/pytorch/issues/112577.
+    if query_states.device.type == "cuda" and causal_mask is not None:
+        query_states = query_states.contiguous()
+        key_states = key_states.contiguous()
+        value_states = value_states.contiguous()
+
+    # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+    # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+    is_causal = True if causal_mask is None and q_len > 1 else False
+
+    attn_output = ulysses_forward(
+        query_states,
+        key_states,
+        value_states,
+        attn_mask=causal_mask,
+        dropout_p=self.attention_dropout if self.training else 0.0,
+        is_causal=is_causal,
     )
-    hidden_states = residual + hidden_states
 
-    # Fully Connected
-    residual = hidden_states
-    hidden_states = self.post_attention_layernorm(hidden_states)
-    hidden_states = self.mlp(hidden_states)
-    hidden_states = residual + hidden_states
+    attn_output = attn_output.transpose(1, 2).contiguous()
+    attn_output = attn_output.view(bsz, q_len, -1)
 
-    outputs = (hidden_states,)
+    attn_output = self.o_proj(attn_output)
 
-    if output_attentions:
-        outputs += (self_attn_weights,)
-
-    if use_cache:
-        outputs += (present_key_value,)
-
-    return outputs
+    return attn_output, None, past_key_value
 
 
-def apply_ulysses_attn_monkey_patch_llama():
-    transformers.models.llama.modeling_llama.LlamaFlashAttention2._flash_attention_forward = (
-        new_flash_attn_forward
-    )
-    transformers.models.llama.modeling_llama.LlamaDecoderLayer.forward = (
-        new_decoder_forward
-    )
+def apply_ulysses_attn_patch_llama(model):
+    for i in range(model.config.num_hidden_layers):
+        import types
+        model.model.layers[i].self_attn.forward = types.MethodType(new_attn_forward, model.model.layers[i].self_attn)
+    print("Applied TP-SP patch for LlamaFlashAttn")
